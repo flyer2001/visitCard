f5eda1c3b0a9ebc246696a6321ab473ecd60d12d
Sergey Popyvanov <s.popyvanov@gmail.com>
2022-06-10 20:01:09 +0400

    tags

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -125,6 +125,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
